9b7bf23a8610daf2a8f9257a4e38b23774b9305d
Jonathan Andrés Pardo <andres.961221@gmail.com>
2021-04-28 20:53:41 -0500

    create file for !2021-04-28

--- commitfile.py
+++ commitfile.py
@@ -2,3 +2,4 @@ from datetime import date
 import subprocess
 subprocess.run(['git', 'add','.'])
 subprocess.run(['git', 'commit', '-m', 'create file for !'+str(  date.today())])
+subprocess.run(['git', 'push', 'origin/master'])
